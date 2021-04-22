22c5e1ee7845b123c8001ed3d869974a06c63fc8
Ihor Buhun <ihor.bugun@statystech.ca>
2021-04-22 22:22:45 +0300

    testRunner update to read propperties in case of bad input data

--- src/test/java/statystech/aqaframework/tests/TestRunner.java
+++ src/test/java/statystech/aqaframework/tests/TestRunner.java
@@ -6,19 +6,24 @@ import statystech.aqaframework.tests.TestRail.TestSuiteID;
 import statystech.aqaframework.utils.DataUtils;
 
 import java.io.IOException;
+import java.util.Properties;
 
 public class TestRunner {
 
     @Test
     public void testRunner() throws IOException {
-//        String runID = System.getProperty( "runID" );
-//        String suiteID = System.getProperty( "suiteID" );
-        String runID = "7";
-        String suiteID = "1";
+
+        String runID = System.getProperty("runID");
+        String suiteID = System.getProperty("suiteID");
         System.out.println("runID: " + runID);
         System.out.println("suiteID: " + suiteID);
-        DataUtils.updateTestRailPropertyParameter("testrail_runId", runID);
-        DataUtils.updateTestRailPropertyParameter("testrail_testSuiteId", suiteID);
+        if (runID != null & suiteID != null) {
+            DataUtils.updateTestRailPropertyParameter("testrail_runId", runID);
+            DataUtils.updateTestRailPropertyParameter("testrail_testSuiteId", suiteID);
+        } else {
+            Properties properties = DataUtils.getProperty("test_rail_config.properties");
+            suiteID = properties.getProperty("testrail_testSuiteId").trim();
+        }
         Class<?> testClass = new TestSuiteID().getTestClass(suiteID);
         JUnitCore.runClasses(testClass);
     }

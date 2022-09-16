152cfad320f9fb81f0f1a9a393c2a8fad1ff8279
Ihor Buhun <ihor.bugun@statystech.ca>
2022-09-16 22:47:54 +0200

    fixes

--- src/resources/SQL/cleanup_order9993305.sql
+++ src/resources/SQL/cleanup_order9993305.sql
@@ -5,15 +5,15 @@ delete from lwa_test_enc.warehouseTransaction
 where orderID= (SELECT orderID from lwa_test_enc.orders where orderAllSysID = 9993305);
 
 delete from lwa_test_enc.parcelLine where orderLineID in (
-SELECT orderLineID FROM lwa_test_enc.orderLine WHERE warehouseOrderID = (
+SELECT orderLineID FROM lwa_test_enc.orderLine WHERE warehouseOrderID in (
 SELECT warehouseOrderID FROM lwa_test_enc.warehouseOrder WHERE orderID = (SELECT orderID from lwa_test_enc.orders where orderAllSysID = 9993305)));
 
 delete FROM lwa_test_enc.orderLine
-where warehouseOrderID = (SELECT warehouseOrderID from lwa_test_enc.warehouseOrder
+where warehouseOrderID in (SELECT warehouseOrderID from lwa_test_enc.warehouseOrder
 where orderID = (SELECT orderID from lwa_test_enc.orders where orderAllSysID = 9993305));
 
 delete FROM lwa_test_enc.parcel
-where warehouseOrderID = (SELECT warehouseOrderID from lwa_test_enc.warehouseOrder
+where warehouseOrderID in (SELECT warehouseOrderID from lwa_test_enc.warehouseOrder
 where orderID = (SELECT orderID from lwa_test_enc.orders where orderAllSysID = 9993305));
 
 delete FROM lwa_test_enc.warehouseOrder
